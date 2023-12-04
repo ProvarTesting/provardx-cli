72ed94ae1daf84e3fc6fb8e1f7b6cd231eb0cbfb
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2023-12-04 17:07:46 +0530

    PDX-66 : Done refactoring for some code in permission.nut file

--- test/commands/sf/provar/config/permission.nut.ts
+++ test/commands/sf/provar/config/permission.nut.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs';
-import * as os from 'os';
 import { exec } from 'child_process';
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
@@ -14,21 +13,13 @@ describe('Handling Insufficient Permissions scenarios as write permission is rem
   });
 
   if (process.platform === 'win32') {
-    const platform: string = os.platform();
-    // eslint-disable-next-line no-console
-    console.log(platform);
-
     it('Boilerplate json file should not be generated inside InsufficientPermission folder with "-p" flag', (done) => {
       const folderPath = './test/InsufficientPermission';
       const command = `C:/Windows/System32/icacls "${folderPath}" /deny "Everyone:(WD)"`;
-      exec(command, (error, stdout, stderr) => {
+      exec(command, (error) => {
         if (error) {
-          // eslint-disable-next-line no-console
-          console.error(`Error removing write permission: ${stderr}`);
           done(error);
         } else {
-          // eslint-disable-next-line no-console
-          console.log(`Write permission removed: ${stdout}`);
           const res = execCmd<SfProvarConfigGenerateResult>(
             'sf provar config generate -p ./test/InsufficientPermission/bin.json'
           ).shellOutput;
@@ -43,21 +34,16 @@ describe('Handling Insufficient Permissions scenarios as write permission is rem
     it('Boilerplate json file should not be generated with "-p" flag as Invalid Path, Extension and Insufficient Permissions', (done) => {
       const folderPath = './test/InsufficientPermission';
       const command = `C:/Windows/System32/icacls "${folderPath}" /deny "Everyone:(WD)"`;
-      exec(command, (error, stdout, stderr) => {
+      exec(command, (error) => {
         if (error) {
-          // eslint-disable-next-line no-console
-          console.error(`Error removing write permission: ${stderr}`);
           done(error);
         } else {
-          // eslint-disable-next-line no-console
-          console.log(`Write permission removed: ${stdout}`);
           const res = execCmd<SfProvarConfigGenerateResult>(
             'sf provar config generate -p ./test/InsufficientPermission/cd/Dom.uu'
           ).shellOutput;
           expect(res.stderr).to.deep.equal(
             'Error (1): INVALID_FILE_EXTENSION - Only the .json file extension is supported.\n'
           );
-
           done();
         }
       });
@@ -66,14 +52,10 @@ describe('Handling Insufficient Permissions scenarios as write permission is rem
     it('Boilerplate json file should not be generated inside InsufficientPermission folder with "--properties-file" flag and return the result in json format', (done) => {
       const folderPath = './test/InsufficientPermission';
       const command = `C:/Windows/System32/icacls "${folderPath}" /deny "Everyone:(WD)"`;
-      exec(command, (error, stdout, stderr) => {
+      exec(command, (error) => {
         if (error) {
-          // eslint-disable-next-line no-console
-          console.error(`Error removing write permission: ${stderr}`);
           done(error);
         } else {
-          // eslint-disable-next-line no-console
-          console.log(`Write permission removed: ${stdout}`);
           const result = execCmd<SfProvarConfigGenerateResult>(
             'sf provar config generate --properties-file ./test/InsufficientPermission/new.json --json',
             {
@@ -89,14 +71,10 @@ describe('Handling Insufficient Permissions scenarios as write permission is rem
     it('Boilerplate json file should not be generated with "-p" flag as Invalid Path and Insufficient Permission and return the result in json format', (done) => {
       const folderPath = './test/InsufficientPermission';
       const command = `C:/Windows/System32/icacls "${folderPath}" /deny "Everyone:(WD)"`;
-      exec(command, (error, stdout, stderr) => {
+      exec(command, (error) => {
         if (error) {
-          // eslint-disable-next-line no-console
-          console.error(`Error removing write permission: ${stderr}`);
           done(error);
         } else {
-          // eslint-disable-next-line no-console
-          console.log(`Write permission removed: ${stdout}`);
           const result = execCmd<SfProvarConfigGenerateResult>(
             'sf provar config generate -p ./test/InsufficientPermission/u/unit.json --json',
             {
@@ -109,9 +87,6 @@ describe('Handling Insufficient Permissions scenarios as write permission is rem
       });
     });
   } else if (process.platform === 'linux') {
-    const platform: string = os.platform();
-    // eslint-disable-next-line no-console
-    console.log(platform);
     it('Boilerplate json file should not be generated inside InsufficientPermission folder with "--properties-file" flag', () => {
       const folderPath = './test/InsufficientPermission';
       fs.chmodSync(folderPath, '555');
