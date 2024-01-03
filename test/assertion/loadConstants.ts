6e71ee01e2f097a46b5f2e261b3823efa8986229
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-01-03 18:22:47 +0530

    PDX-67 Adding load success scenarios in nuts file

---
+++ test/assertion/loadConstants.ts
@@ -0,0 +1,10 @@
+export const sfProvarConfigLoadCommand = 'sf provar config load';
+export const loadSuccessMessage = 'The properties file was loaded successfully.\n';
+
+export const loadSuccessJson = {
+  status: 0,
+  result: {
+    success: true,
+  },
+  warnings: [],
+};
--- test/commands/sf/provar/config/load.nut.ts
+++ test/commands/sf/provar/config/load.nut.ts
@@ -1,21 +1,28 @@
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
+import { SfProvarCommandResult } from '../../../../../src/Utility/sfProvarCommandResult';
+import { sfProvarConfigGenerateCommand } from '../../../../assertion/generateConstants';
+import * as loadConstants from '../../../../assertion/loadConstants';
 
 describe('sf provar config load NUTs', () => {
   let session: TestSession;
-
-  before(async () => {
-    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-  });
 
   after(async () => {
     await session?.clean();
   });
 
-  it('should display provided name', () => {
-    const name = 'World';
-    const command = `sf provar config load --name ${name}`;
-    const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
-    expect(output).to.contain(name);
+  it('Boilerplate json file should be loaded successfully and return a success message', () => {
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p loadFile.json`);
+    const res = execCmd<SfProvarCommandResult>(`${loadConstants.sfProvarConfigLoadCommand} -p loadFile.json`, {
+      ensureExitCode: 0,
+    }).shellOutput;
+    expect(res.stdout).to.deep.equal(loadConstants.loadSuccessMessage);
+  });
+
+  it('Boilerplate json file should be loaded successfully and return a success message in json format', () => {
+    const res = execCmd<SfProvarCommandResult>(`${loadConstants.sfProvarConfigLoadCommand} -p loadFile.json --json`, {
+      ensureExitCode: 0,
+    });
+    expect(res.jsonOutput).to.deep.equal(loadConstants.loadSuccessJson);
   });
 });
