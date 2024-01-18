44953ea1f4a1f70d45a96eb880d4b72614c49344
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-01-18 10:49:45 +0530

    PDX-65 Adding positive scenarios in set Nuts

---
+++ test/assertion/setConstants.ts
@@ -0,0 +1,8 @@
+export const sfProvarConfigSetCommand = 'sf provar config set';
+export const setSuccessJson = {
+  status: 0,
+  result: {
+    success: true,
+  },
+  warnings: [],
+};
--- test/commands/sf/provar/config/set.nut.ts
+++ test/commands/sf/provar/config/set.nut.ts
@@ -1,21 +1,47 @@
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
+import { SfProvarCommandResult } from '../../../../../src/Utility/sfProvarCommandResult';
+import { sfProvarConfigGenerateCommand } from '../../../../assertion/generateConstants';
+import { sfProvarConfigLoadCommand } from '../../../../assertion/loadConstants';
+import { sfProvarConfigValidateCommand } from '../../../../assertion/validateConstants';
+import { sfProvarConfigSetCommand } from '../../../../assertion/setConstants';
+import * as setConstants from '../../../../assertion/setConstants';
+// import { errorMessages } from '../../../../../src/constants/errorMessages';
 
 describe('sf provar config set NUTs', () => {
   let session: TestSession;
-
-  before(async () => {
-    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-  });
 
   after(async () => {
     await session?.clean();
   });
 
-  it('should display provided name', () => {
-    const name = 'World';
-    const command = `sf provar config set --name ${name}`;
-    const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
-    expect(output).to.contain(name);
+  it('Value should be set successfully for environment property in json file', () => {
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p setEnvValue.json`);
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigLoadCommand} -p setEnvValue.json`);
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigValidateCommand}`);
+    const res = execCmd<SfProvarCommandResult>(
+      `${sfProvarConfigSetCommand} environment.testEnvironment='"SIT"'`
+    ).shellOutput;
+    expect(res.stdout).to.deep.equal('');
+  });
+
+  it('Value should be set successfully for environment property in json file in json format', () => {
+    const res = execCmd<SfProvarCommandResult>(
+      `${sfProvarConfigSetCommand} environment.testEnvironment='"TEST"' --json`,
+      {
+        ensureExitCode: 0,
+      }
+    );
+    expect(res.jsonOutput).to.deep.equal(setConstants.setSuccessJson);
+  });
+
+  it('Value should be set successfully for environment property in json file', () => {
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p setTestCases.json`);
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigLoadCommand} -p setTestCases.json`);
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigValidateCommand}`);
+    const res = execCmd<SfProvarCommandResult>(
+      `${sfProvarConfigSetCommand} testCases='["tests/myTestCase.testcase","tests/testSuite1/myTestCase1.testCase"]' --json`
+    );
+    expect(res.jsonOutput).to.deep.equal(setConstants.setSuccessJson);
   });
 });
