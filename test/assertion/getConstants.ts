35ffc43abb53ef6e08891664ebfd9132426cb2c7
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-01-23 13:20:24 +0530

    PDX-65 Adding detail get nuts

--- test/assertion/getConstants.ts
+++ test/assertion/getConstants.ts
@@ -6,3 +6,55 @@ export const getSuccessJson = {
   },
   warnings: [],
 };
+
+export const getResultsPathValue = {
+  status: 0,
+  result: {
+    success: true,
+    value: 'C:/Users/anchal.goel/git/ProvarRegressionqam5/test/provardx/Results',
+  },
+  warnings: [],
+};
+
+export const getEnvironmentJsonObject = {
+  status: 0,
+  result: {
+    success: true,
+    value: {
+      testEnvironment: '${PROVAR_TEST_ENVIRONMENT}',
+      webBrowser: 'Chrome',
+      webBrowserConfig: 'Full Screen',
+      webBrowserProviderName: 'Desktop',
+      webBrowserDeviceName: 'Full Screen',
+    },
+  },
+  warnings: [],
+};
+
+export const missingPropertyGetJson = {
+  status: 0,
+  result: {
+    success: false,
+    errors: [
+      {
+        code: 'MISSING_PROPERTY',
+        message: 'Please, specify a property to get from the properties file.',
+      },
+    ],
+  },
+  warnings: [],
+};
+
+export const unknownPropertyJson = {
+  status: 0,
+  result: {
+    success: false,
+    errors: [
+      {
+        code: 'UNKNOWN_PROPERTY',
+        message: 'The property is not present in the file.',
+      },
+    ],
+  },
+  warnings: [],
+};
--- test/commands/sf/provar/config/get.nut.ts
+++ test/commands/sf/provar/config/get.nut.ts
@@ -4,50 +4,166 @@ import { expect } from 'chai';
 import { SfProvarCommandResult } from '../../../../../src/Utility/sfProvarCommandResult';
 import { sfProvarConfigGenerateCommand } from '../../../../assertion/generateConstants';
 import { sfProvarConfigLoadCommand } from '../../../../assertion/loadConstants';
-import { sfProvarConfigValidateCommand } from '../../../../assertion/validateConstants';
+import * as validateConstants from '../../../../assertion/validateConstants';
 import * as getConstants from '../../../../assertion/getConstants';
+import * as setConstants from '../../../../assertion/setConstants';
 import { errorMessages } from '../../../../../src/constants/errorMessages';
 
-describe('sf provar config set NUTs', () => {
+describe('sf provar config get NUTs', () => {
   let session: TestSession;
 
   after(async () => {
     await session?.clean();
+    const filePaths = ['getinvalidFile.json', 'getValues.json'];
+    filePaths.forEach((filePath) => {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          return err;
+        }
+      });
+    });
   });
 
-  it('Value should be get successfully for environment property in json file and return the result', () => {
-    execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p getEnvValue.json`);
-    execCmd<SfProvarCommandResult>(`${sfProvarConfigLoadCommand} -p getEnvValue.json`);
-    execCmd<SfProvarCommandResult>(`${sfProvarConfigValidateCommand}`);
-    interface PropertyFileJsonData {
-      environment: {
-        testEnvironment: string;
-      };
-    }
-    const jsonFilePath = 'getEnvValue.json';
-    const jsonData: PropertyFileJsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8')) as PropertyFileJsonData;
-    const testEnvironmentValue = jsonData.environment.testEnvironment;
-    const res = execCmd<SfProvarCommandResult>(
-      `${getConstants.sfProvarConfigGetCommand} environment.testEnvironment`
-    ).shellOutput;
-    expect(res.stdout).to.deep.equal(testEnvironmentValue);
-  });
-
-  it('Value should be get successfully for metdata property in json file and return result in json format', () => {
-    const res = execCmd<SfProvarCommandResult>(
-      `${getConstants.sfProvarConfigGetCommand} metadata.metadataLevel --json`,
-      {
-        ensureExitCode: 0,
-      }
-    ).jsonOutput;
-    expect(res?.result.success).to.deep.equal('true');
-    // expect(res?.result.value).to.deep.equal('Reuse');
-  });
-
-  it('Value should not be get if json file does not exists in sf config file', () => {
+  it('Property value is not returned as json file is not loaded', () => {
     execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p getinvalidFile.json`);
+    const jsonFilePath = 'getinvalidFile.json';
+    const data = fs.readFileSync(jsonFilePath, 'utf-8');
+    const newData = data.substring(1);
+    fs.writeFile(jsonFilePath, newData, (error) => {
+      if (error) {
+        return;
+      }
+    });
     execCmd<SfProvarCommandResult>(`${sfProvarConfigLoadCommand} -p getinvalidFile.json`);
     const res = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} provarHome`).shellOutput;
     expect(res.stderr).to.deep.equal(`Error (1): [MISSING_FILE] ${errorMessages.MISSINGFILEERROR}\n`);
+  });
+
+  it('Property value is not returned as json file is not loaded and return the error in json format', () => {
+    const res = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} resultsPath --json`, {
+      ensureExitCode: 0,
+    });
+    expect(res.jsonOutput).to.deep.equal(validateConstants.missingFileJsonError);
+  });
+
+  it('Property name is missing', () => {
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigGenerateCommand} -p getValues.json`);
+    execCmd<SfProvarCommandResult>(`${sfProvarConfigLoadCommand} -p getValues.json`);
+    execCmd<SfProvarCommandResult>(`${validateConstants.sfProvarConfigValidateCommand}`);
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand}`).shellOutput;
+    expect(getOutput.stderr).to.deep.equal(`Error (1): [MISSING_PROPERTY] ${errorMessages.MISSING_PROPERTY_GET}\n`);
+  });
+
+  it('Property name is missing and return the error in json format', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} --json`).jsonOutput;
+    expect(getOutput).to.deep.equal(getConstants.missingPropertyGetJson);
+  });
+
+  it('Property name to be returned for metadata obj is not present in the file and return the error', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} metadata.test`
+    ).shellOutput;
+    expect(getOutput.stderr).to.deep.equal(`Error (1): [UNKNOWN_PROPERTY] ${errorMessages.UNKNOWN_PROPERTY}\n`);
+  });
+
+  it('Property name to be returned for env object is not present in the file and return the error', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} environment.webBrowserProvider`
+    ).shellOutput;
+    expect(getOutput.stderr).to.deep.equal(`Error (1): [UNKNOWN_PROPERTY] ${errorMessages.UNKNOWN_PROPERTY}\n`);
+  });
+
+  it('Property name to be returned not present in the file and return the error', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} projectPath.web`
+    ).shellOutput;
+    expect(getOutput.stderr).to.deep.equal(`Error (1): [UNKNOWN_PROPERTY] ${errorMessages.UNKNOWN_PROPERTY}\n`);
+  });
+
+  it('Property name to be returned is not present in the file and return the error in json format', () => {
+    const res = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} sample --json`, {
+      ensureExitCode: 0,
+    });
+    expect(res.jsonOutput).to.deep.equal(getConstants.unknownPropertyJson);
+  });
+
+  it('value should be returned for provarHome property', () => {
+    const setProvarHome = 'C:/Users/anchal.goel/Downloads/main_win64_e413157177_20240117_0452/';
+    execCmd<SfProvarCommandResult>(`${setConstants.sfProvarConfigSetCommand} provarHome=${setProvarHome}`);
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} provarHome`).shellOutput;
+    expect(getOutput.stdout).to.deep.equal(`${setProvarHome}\n`);
+  });
+
+  it('value should be returned for projectPath property', () => {
+    const setprojectPath = 'C:/Users/anchal.goel/Desktop/main_win64_e413157177_20240117_0452/';
+    const Output = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} projectPath`).shellOutput;
+    expect(Output.stdout).to.deep.equal('${PROVAR_PROJECT_PATH}\n');
+    execCmd<SfProvarCommandResult>(`${setConstants.sfProvarConfigSetCommand} projectPath=${setprojectPath}`);
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} "projectPath"`
+    ).shellOutput;
+    expect(getOutput.stdout).to.deep.equal(`${setprojectPath}\n`);
+  });
+
+  it('value should be returned for resultsPath property in json format', () => {
+    execCmd<SfProvarCommandResult>(
+      `${setConstants.sfProvarConfigSetCommand} "resultsPath"="C:/Users/anchal.goel/git/ProvarRegressionqam5/test/provardx/Results"`
+    );
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} resultsPath --json`, {
+      ensureExitCode: 0,
+    });
+    expect(getOutput.jsonOutput).to.deep.equal(getConstants.getResultsPathValue);
+  });
+
+  it('value should be returned for smtpPath property', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} "smtpPath"`).shellOutput;
+    expect(getOutput.stdout).to.deep.equal('\n');
+  });
+
+  it('value should be returned for testOutputLevel property', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} testOutputLevel`
+    ).shellOutput;
+    expect(getOutput.stdout).to.deep.equal('BASIC\n');
+  });
+
+  it('value should be returned for resultsPathDisposition property in json format', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} resultsPathDisposition --json`
+    ).jsonOutput;
+    expect(getOutput?.result.value).to.deep.equal('Increment');
+  });
+
+  it('value should be returned for lightningMode property in json format', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} lightningMode --json`
+    ).jsonOutput;
+    expect(getOutput?.result.value).to.deep.equal(true);
+  });
+
+  it('Value should be returned successfully for metdata object', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} metadata`).shellOutput;
+    expect(getOutput.stdout).to.deep.equal("{ metadataLevel: 'Reuse', cachePath: '../.provarCaches' }\n");
+  });
+
+  it('Value should be returned successfully for environment object in json format', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(`${getConstants.sfProvarConfigGetCommand} environment --json`, {
+      ensureExitCode: 0,
+    });
+    expect(getOutput.jsonOutput).to.deep.equal(getConstants.getEnvironmentJsonObject);
+  });
+
+  it('Value should be returned successfully for webBrowserDeviceName property in environment object', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} "environment.webBrowserDeviceName"`
+    ).shellOutput;
+    expect(getOutput.stdout).to.deep.equal('Full Screen\n');
+  });
+
+  it('Value should be returned successfully for testprojectSecrets property', () => {
+    const getOutput = execCmd<SfProvarCommandResult>(
+      `${getConstants.sfProvarConfigGetCommand} testprojectSecrets`
+    ).shellOutput;
+    expect(getOutput.stdout).to.deep.equal('${PROVAR_TEST_PROJECT_SECRETS}\n');
   });
 });
