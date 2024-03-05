745b9fb8efb052b646eb2d23f707023f0f83a42e
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-03-05 15:06:00 +0530

    PDX-206 setup Nuts

--- src/constants/commandConstants.ts
+++ src/constants/commandConstants.ts
@@ -4,4 +4,5 @@ export enum commandConstants {
   SF_PROVAR_CONFIG_VALIDATE_COMMAND = 'provar config validate',
   SF_PROVAR_CONFIG_SET_COMMAND = 'provar config set',
   SF_PROVAR_CONFIG_GET_COMMAND = 'provar config get',
+  SF_PROVAR_AUTOMATION_SETUP_COMMAND = 'provar sutomation setup',
 }
---
+++ test/assertion/setupConstants.ts
@@ -0,0 +1,8 @@
+export const successMessage = 'Provar Automation was set up successfully'
+export const successJsonMessage = {
+  status: 0,
+  result: {
+    success: true,
+  },
+  warnings: [],
+};
