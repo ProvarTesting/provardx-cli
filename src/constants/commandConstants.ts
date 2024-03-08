27b717fe77e89b813af2fb7ba99e3f0166a9fac5
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-03-08 14:55:42 +0530

    PDX-57: adding automation to command name

--- src/constants/commandConstants.ts
+++ src/constants/commandConstants.ts
@@ -4,5 +4,5 @@ export enum commandConstants {
   SF_PROVAR_AUTOMATION_CONFIG_VALIDATE_COMMAND = 'provar automation config validate',
   SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND = 'provar automation config set',
   SF_PROVAR_AUTOMATION_CONFIG_GET_COMMAND = 'provar automation config get',
-  SF_PROVAR_AUTOMATION_METADATA_DOWNLOAD_COMMAND = 'provar metadata download',
+  SF_PROVAR_AUTOMATION_METADATA_DOWNLOAD_COMMAND = 'provar automation metadata download',
 }
