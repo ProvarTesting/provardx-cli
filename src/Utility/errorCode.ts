c38f7271a8e02f5b724df5d6a5ed1fdaca7f1ce3
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-03-12 17:46:13 +0530

    PDX-60: base code for test run

--- src/Utility/errorCode.ts
+++ src/Utility/errorCode.ts
@@ -20,4 +20,5 @@ export type ErrorCode =
   | 'INVALID_ARGUMENT'
   | 'INVALID_PROPERTY'
   | 'UNKNOWN_PROPERTY'
-  | 'DOWNLOAD_ERROR';
+  | 'DOWNLOAD_ERROR'
+  | 'TEST_RUN_ERROR';
--- src/commands/provar/automation/test/run.ts
+++ src/commands/provar/automation/test/run.ts
@@ -1,36 +1,73 @@
-import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import * as fileSystem from 'node:fs';
+import { spawnSync } from 'node:child_process';
+import { SfCommand } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core';
+import { SfProvarCommandResult, populateResult } from '../../../../Utility/sfProvarCommandResult.js';
+import ErrorHandler from '../../../../Utility/errorHandler.js';
+import { ProvarConfig } from '../../../../Utility/provarConfig.js';
+import { errorMessages } from '../../../../constants/errorMessages.js';
+import UserSupport from '../../../../Utility/userSupport.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const messages = Messages.loadMessages('@provartesting/provardx-cli', 'provar.automation.test.run');
 
-export type ProvarAutomationTestRunResult = {
-  path: string;
-};
-
-export default class ProvarAutomationTestRun extends SfCommand<ProvarAutomationTestRunResult> {
+export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandResult> {
   public static readonly summary = messages.getMessage('summary');
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
 
-  public static readonly flags = {
-    name: Flags.string({
-      summary: messages.getMessage('flags.name.summary'),
-      description: messages.getMessage('flags.name.description'),
-      char: 'n',
-      required: false,
-    }),
-  };
-
-  public async run(): Promise<ProvarAutomationTestRunResult> {
+  private errorHandler: ErrorHandler = new ErrorHandler();
+  public async run(): Promise<SfProvarCommandResult> {
     const { flags } = await this.parse(ProvarAutomationTestRun);
 
-    const name = flags.name ?? 'world';
-    this.log(
-      `hello ${name} from D:\\provardx-cli-checkout\\provardx-cli\\src\\commands\\provar\\automation\\test\\run.ts`
-    );
-    return {
-      path: 'D:\\provardx-cli-checkout\\provardx-cli\\src\\commands\\provar\\automation\\test\\run.ts',
-    };
+    this.log('asd');
+
+    const config: ProvarConfig = await ProvarConfig.loadConfig(this.errorHandler);
+    const propertiesFilePath = config.get('PROVARDX_PROPERTIES_FILE_PATH')?.toString();
+
+    if (propertiesFilePath === undefined || !fileSystem.existsSync(propertiesFilePath)) {
+      this.errorHandler.addErrorsToList('MISSING_FILE', errorMessages.MISSINGFILEERROR);
+      return populateResult(flags, this.errorHandler, messages, this.log.bind(this));
+    }
+
+    try {
+      /* eslint-disable */
+      const propertiesdata = fileSystem.readFileSync(propertiesFilePath, { encoding: 'utf8' });
+      const propertiesInstance = JSON.parse(propertiesdata);
+      const rawProperties = JSON.stringify(propertiesInstance);
+      const userSupport = new UserSupport();
+      const updateProperties = userSupport.prepareRawProperties(rawProperties);
+      const userInfo = await userSupport.getDxUsersInfo(propertiesInstance.connectionOverride, this.errorHandler);
+      if (userInfo === null) {
+        return populateResult(flags, this.errorHandler, messages, this.log.bind(this));
+      }
+      const userInfoString = userSupport.prepareRawProperties(JSON.stringify({ dxUsers: userInfo }));
+      const provarDxJarPath = propertiesInstance.provarHome + '/provardx/provardx.jar';
+      const testRunCommand =
+        'java -cp "' +
+        provarDxJarPath +
+        '" com.provar.provardx.DxCommandExecuter ' +
+        updateProperties +
+        ' ' +
+        userInfoString +
+        ' ' +
+        'Runtests';
+      spawnSync(testRunCommand, { shell: true });
+      //  const javaProcessOutput = spawnSync(testRunCommand, { shell: true });
+      // const testRun = '';
+      // if (!fileContainsString(javaProcessOutput.stderr.toString(), testRun)) {
+      //   const errorMessage = getStringAfterSubstring(javaProcessOutput.stderr.toString(), 'ERROR');
+      //   this.errorHandler.addErrorsToList('TEST_RUN_ERROR', `${errorMessage}`);
+      // }
+    } catch (error: any) {
+      if (error.name === 'SyntaxError') {
+        this.errorHandler.addErrorsToList('MALFORMED_FILE', errorMessages.MALFORMEDFILEERROR);
+      } else if (error.name === 'MULTIPLE_ERRORSError') {
+        return populateResult(flags, this.errorHandler, messages, this.log.bind(this));
+      } else {
+        this.errorHandler.addErrorsToList('TEST_RUN_ERROR', `${error.errorMessage}`);
+      }
+    }
+    return populateResult(flags, this.errorHandler, messages, this.log.bind(this));
   }
 }
