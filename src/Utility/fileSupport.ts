649aac65f09751fdc393b8ee3c089e7b90a806b1
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-03-15 13:44:45 +0530

    PDX-206: eslint disable

--- src/Utility/fileSupport.ts
+++ src/Utility/fileSupport.ts
@@ -4,9 +4,9 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.md file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+/* eslint-disable */
 
 import * as fs from 'node:fs';
-// eslint-disable-next-line
 import StreamZip from 'node-stream-zip';
 import { propertyFileContent } from '../constants/propertyFileContent.js';
 
