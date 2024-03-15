9935a4e0bdc24e0c044364c3a3df6a382f71957a
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-03-15 12:00:20 +0530

    PDX-56 adding nuts

--- src/constants/commandConstants.ts
+++ src/constants/commandConstants.ts
@@ -5,4 +5,5 @@ export enum commandConstants {
   SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND = 'provar automation config set',
   SF_PROVAR_AUTOMATION_CONFIG_GET_COMMAND = 'provar automation config get',
   SF_PROVAR_AUTOMATION_METADATA_DOWNLOAD_COMMAND = 'provar automation metadata download',
+  SF_PROVAR_AUTOMATION_PROJECT_COMPILE_COMMAND = 'provar automation project compile'
 }
--- test/commands/provar/automation/project/compile.nut.ts
+++ test/commands/provar/automation/project/compile.nut.ts
@@ -1,21 +1,51 @@
+import * as fileSystem from 'node:fs';
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
+import { SfProvarCommandResult } from '../../../../../src/Utility/sfProvarCommandResult.js';
+import { commandConstants } from '../../../../../src/constants/commandConstants.js';
+import * as compileConstants from '../../../../assertion/compileConstants.js';
 
 describe('provar automation project compile NUTs', () => {
   let session: TestSession;
-
-  before(async () => {
-    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-  });
+  enum FILE_PATHS {
+    COMPILE_FILE = 'metadataDownloadFile.json',
+  }
 
   after(async () => {
     await session?.clean();
+    Object.values(FILE_PATHS).forEach((filePath) => {
+      fileSystem.unlink(filePath, (err) => {
+        if (err) {
+          return err;
+        }
+      });
+    });
   });
 
-  it('should display provided name', () => {
-    const name = 'World';
-    const command = `provar automation project compile --name ${name}`;
-    const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
-    expect(output).to.contain(name);
+  it('Compile command should be successful', () => {
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_GENERATE_COMMAND} -p ${FILE_PATHS.COMPILE_FILE}`
+    );
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_LOAD_COMMAND} -p ${FILE_PATHS.COMPILE_FILE}`
+    );
+    const SET_PROVAR_HOME_VALUE = '"C:/Program Files/Provar/2.12.1.1.02/"';
+    const SET_PROJECT_PATH_VALUE = '"D:/Provar Workspace/8Feb/Provar"';
+    // set provarHome and projectPath locations
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND} "provarHome"=${SET_PROVAR_HOME_VALUE}`
+    );
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND} "projectPath"=${SET_PROJECT_PATH_VALUE}`
+    );
+    const result = execCmd<SfProvarCommandResult>(`${commandConstants.SF_PROVAR_AUTOMATION_PROJECT_COMPILE_COMMAND}`).shellOutput;
+    expect(result.stdout).to.deep.equal(compileConstants.successMessage);
+  });
+
+  it('Compile command should be successful and return the result in json', () => {
+    const result = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_PROJECT_COMPILE_COMMAND} --json`
+    ).jsonOutput;
+    expect(result).to.deep.equal(compileConstants.successJsonMessage);
   });
 });
