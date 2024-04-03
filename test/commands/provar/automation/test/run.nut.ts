7bb03c12f7aad4ce1a38ac230e95888fd5943510
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-04-03 11:57:54 +0530

    PDX-60 Adding sceanrios

--- test/commands/provar/automation/test/run.nut.ts
+++ test/commands/provar/automation/test/run.nut.ts
@@ -1,21 +1,81 @@
+import * as fileSystem from 'node:fs';
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
+import { SfProvarCommandResult } from '../../../../../src/Utility/sfProvarCommandResult.js';
+import { commandConstants } from '../../../../../src/constants/commandConstants.js';
+import { errorMessages } from '../../../../../src/constants/errorMessages.js';
+import * as validateConstants from '../../../../assertion/validateConstants.js';
 
 describe('provar automation test run NUTs', () => {
   let session: TestSession;
-
-  before(async () => {
-    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-  });
+  enum FILE_PATHS {
+    MISSING_FILE = 'missingFile.json',
+    TEST_RUN = 'testRun.json',
+  }
 
   after(async () => {
     await session?.clean();
+    Object.values(FILE_PATHS).forEach((filePath) => {
+      fileSystem.unlink(filePath, (err) => {
+        if (err) {
+          return err;
+        }
+      });
+    });
   });
 
-  it('should display provided name', () => {
-    const name = 'World';
-    const command = `provar automation test run --name ${name}`;
-    const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
-    expect(output).to.contain(name);
+  it('Boilerplate json file should not be compiled if the file has not been loaded', () => {
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_GENERATE_COMMAND} -p ${FILE_PATHS.MISSING_FILE}`
+    );
+    interface PropertyFileJsonData {
+      [key: string]: string | boolean | number;
+    }
+    const jsonFilePath = 'MissingFile.json';
+    // reading the json data
+    const jsonDataString = fileSystem.readFileSync(jsonFilePath, 'utf-8');
+    const jsonData: PropertyFileJsonData = JSON.parse(jsonDataString) as PropertyFileJsonData;
+    jsonData.provarHome = '';
+    jsonData.projectPath = '';
+    const updatedJsonDataString = JSON.stringify(jsonData, null, 2);
+    fileSystem.writeFileSync(jsonFilePath, updatedJsonDataString, 'utf-8');
+    execCmd<SfProvarCommandResult>(`${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_LOAD_COMMAND}`);
+    const res = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND}`
+    ).shellOutput;
+    expect(res.stderr).to.deep.equal(`Error (1): [MISSING_FILE] ${errorMessages.MISSINGFILEERROR}\n\n`);
   });
+
+  it('Boilerplate json file should not be compiled if the file has not been loaded and return result in json format', () => {
+    const res = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND} --json`,
+      {
+        ensureExitCode: 0,
+      }
+    );
+    expect(res.jsonOutput).to.deep.equal(validateConstants.missingFileJsonError);
+  });
+
+  it('Test Run command should be successful', () => {
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_GENERATE_COMMAND} -p ${FILE_PATHS.TEST_RUN}`
+    );
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_LOAD_COMMAND} -p ${FILE_PATHS.TEST_RUN}`
+    );
+    const SET_PROVAR_HOME_VALUE = '"C:/Program Files/Provar/2.12.1.1.02/"';
+    const SET_PROJECT_PATH_VALUE = '"D:/Provar Workspace/8Feb/Provar"';
+    // set provarHome and projectPath locations
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND} "provarHome"=${SET_PROVAR_HOME_VALUE}`
+    );
+    execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_CONFIG_SET_COMMAND} "projectPath"=${SET_PROJECT_PATH_VALUE}`
+    );
+    const result = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND}`
+    ).shellOutput;
+    expect(result.stdout).to.deep.equal('');
+  });
+
 });
