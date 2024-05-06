a8f3f354bbdbf2a106326c2fc0398c66afb15cc5
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-05-06 15:25:16 +0530

    PDx-287: spelling check

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 const command = 'echo y | sfdx plugins:install @provartesting/provardx-plugins-automation';
 
-exec(command, (error, stdout, stderr) => {
+exec(command, (error) => {
   if (error) {
     console.error(`Error: ${error.message}`);
     return;
