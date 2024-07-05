65fcde90e87c4287e5de5ec1f74312e462b20407
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-07-05 14:15:45 +0530

    changes in postinstall and version changes

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,13 +1,11 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 
 try {
   const command = 'echo y | sf plugins install @provartesting/provardx-plugins-automation';
-  execSync(command, { stdio: 'inherit' });
+  exec(command, { stdio: 'inherit' });
 
-  const commandm = 'echo y | sf plugins install @provartesting/provardx-plugins-manager';
-  execSync(commandm, { stdio: 'inherit' });
+  // const commandm = 'echo y | sf plugins install @provartesting/provardx-plugins-manager';
+  // execSync(commandm, { stdio: 'inherit' });
 } catch (error) {
-  let textDecoder = new TextDecoder('utf-8');
-  const errormsg = textDecoder.decode(error.message);
-  console.error(`Error: ${errormsg}`);
+  console.error(`Error: ${error.message}`);
 }
