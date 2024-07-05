b1e76cf43ca46cef9f7ef98ffec3945613fe01cd
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-07-05 14:38:26 +0530

    version change and script changes

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -2,9 +2,12 @@ import { exec, execSync } from 'child_process';
 
 try {
   const command = 'echo y | sf plugins install @provartesting/provardx-plugins-automation';
-  // exec(command, { stdio: 'inherit' });
-  const commandOutpit = execSync(command)
-  console.log(commandOutpit);
+  const commandOutput = execSync(command);
+  console.log(commandOutput);
+  const commandm = 'echo y | sf plugins install @provartesting/provardx-plugins-manager';
+  const commandOutputm = execSync(commandm)
+  console.log(commandOutputm);
+
   // const commandm = 'echo y | sf plugins install @provartesting/provardx-plugins-manager';
   // execSync(commandm, { stdio: 'inherit' });
 } catch (error) {
