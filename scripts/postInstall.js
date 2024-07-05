5ccbc2374829bc908819c13dfc0c6faa3695c15b
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-07-05 14:49:46 +0530

    changes in postinstall and version changes

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,9 +1,14 @@
-import { exec, execSync } from 'child_process';
+import { execSync } from 'child_process';
 
 try {
+  console.log('starting postinstall');
+  console.log('starting automation');
+
   const command = 'echo y | sf plugins install @provartesting/provardx-plugins-automation';
   const commandOutput = execSync(command);
   console.log(commandOutput);
+  console.log('starting manager');
+
   const commandm = 'echo y | sf plugins install @provartesting/provardx-plugins-manager';
   const commandOutputm = execSync(commandm)
   console.log(commandOutputm);
