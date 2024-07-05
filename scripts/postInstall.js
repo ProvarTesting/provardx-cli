574d1fec2f76c476311c51d5be1689ef4dbe0750
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-07-05 13:08:36 +0530

    changes in postinstall and version hcange

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,8 +1,16 @@
 import { exec } from 'child_process';
 
 const command = 'echo y | sfdx plugins:install @provartesting/provardx-plugins-automation';
+const commandm = 'echo y | sfdx plugins:install @provartesting/provardx-plugins-manager';
 
 exec(command, (error) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+});
+
+exec(commandm, (error) => {
   if (error) {
     console.error(`Error: ${error.message}`);
     return;
